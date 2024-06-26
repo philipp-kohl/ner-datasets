12f8d20451ac5ebda89561dfcd4dcef846965942
Yokiball <yoka.kraemer@gmx.de>
2024-06-26 12:38:19 +0200

    Add helper functions

---
+++ conll2json.py
@@ -0,0 +1,112 @@
+import pandas as pd
+import json
+import spacy
+from spacy.tokens import Doc
+from spacy.training import biluo_tags_to_spans, iob_to_biluo
+
+train_size = 0.7
+dev_size = 0.15
+test_size = 0.15
+"""
+  Class to convert CoNLL NER data to JSONLine.
+"""
+
+class CONLL2JSON:
+    def __init__(self):
+        self._sep_list = [' ', '\t']
+        self._fmt_list = ['json', 'jsonl']
+
+    @staticmethod
+    def _load_text(txt_path):
+        """
+        Opens the container and reads file
+        Returns a list[string]
+        :param txt_path: filepath
+        """
+        with open(txt_path, 'rt') as infile:
+            content = infile.readlines()
+
+        return content
+
+    @staticmethod
+    def _process_text(content, sep):
+        """
+        given a list of txt_paths
+        -process each
+        :param content: list of strings
+        :param sep: string representing separator
+        :return: list of dicts
+        """
+        list_dicts = []
+        words = []
+        nlp = spacy.blank("en")
+        # detect newline and make sentences
+        for line_num, line in enumerate(content):
+            if line != '\n':
+                words.append(line.strip('\n'))
+            else:
+                sentence = " ".join([word.split(sep)[0] for word in words])
+                words = [word.split(sep) for word in words]
+
+                df_words = pd.DataFrame(data=words, columns=['word', 'ner'])
+                
+                doc = nlp(sentence)
+                tokens = [token.text for token in doc]
+                spaces = len(words) * [True]
+                doc = Doc(nlp.vocab, words=df_words['word'], spaces=spaces)
+
+                tmp = biluo_tags_to_spans(doc, iob_to_biluo(df_words['ner']))
+
+                char_spans = [(span.start_char, span.end_char, span.label_) for span in tmp]
+
+                list_dicts.append({'tokens': tokens, 'text': sentence, 'labels': char_spans})
+
+                words = []
+        return list_dicts
+
+    @staticmethod
+    def _write_text(list_dicts, fmt, output_file):
+        """
+        :param list_dicts: list of dicts formatted in json
+        :param fmt: format of json file either JSON object or JSON Line file
+        :param output_file: file to save data
+        :return:
+        """
+
+    def parse(self, input_file: str, output_file: str, sep: str, fmt) -> None:
+        if sep not in self._sep_list:
+            raise RuntimeError(f'Separator should be in "{self._sep_list}", provided separator was "{sep}"')
+
+        if fmt not in self._fmt_list:
+            raise RuntimeError(f'Format should be in "{self._fmt_list}", provided file format was "{fmt}"')
+
+        content = self._load_text(input_file)
+        list_dicts = self._process_text(content, sep)
+        if fmt == 'json':
+                data = json.dumps(list_dicts)
+        else:
+            list_dicts = [json.dumps(l) for l in list_dicts]
+            data = '\n'.join(list_dicts)
+
+        with open(output_file, 'w') as json_file:
+            json_file.write(data)
+        """
+        With train test dev split:
+        train, rest = train_test_split(list_dicts, train_size=train_size, test_size=1-train_size)
+        test, dev = train_test_split(rest, train_size=test_size/(test_size+dev_size), test_size=dev_size/(test_size+dev_size))
+        sets = [{"train": train}, {"test": test},{"dev": dev}]
+
+        for set in sets:
+            key = list(set.keys())[0]
+            if fmt == 'json':
+                data = json.dumps(set[key])
+            else:
+                list_dicts = [json.dumps(l) for l in set[key]]
+                data = '\n'.join(list_dicts)
+
+            with open(output_file+"_"+key+".jsonl", 'w') as json_file:
+                json_file.write(data)"""
+ 
+def process_conll_format_to_jsonl(input_path: str, output_path: str):
+    converter = CONLL2JSON()
+    converter.parse(input_path, output_path, '\t', 'jsonl')
---
+++ main.py
@@ -0,0 +1,98 @@
+from sklearn.model_selection import train_test_split
+from typing import List
+import srsly
+import re
+import spacy
+import pandas as pd
+from spacy.tokens import Doc
+from spacy.training import biluo_tags_to_spans, iob_to_biluo
+
+
+train_size = 0.7
+dev_size = 0.15
+test_size = 0.15
+
+def process_and_split_local_corpus(dataset, output_name):
+    result = []
+
+    raw_text = dataset.read().strip()
+    raw_docs = re.split(r'\n\t?\n', raw_text)
+
+    for doc in raw_docs:
+        tokens = []
+        tags = []
+        for line in doc.split('\n'):
+            token, tag = line.split('\t')
+            tokens.append(token)
+            tags.append(tag)
+        text = " ".join(tokens)
+
+        labels: List[(int,int,str)] = []
+        count_character: int = 0
+        for i,token in enumerate(tokens):
+            start: int = count_character
+            count_character += len(token)+1 # Add number of character plus whitespace
+            end: int = count_character
+
+            if tags[i] != 'O':
+                labels.append((start,end,tags[i]))
+
+        datapoint = {
+            "tokens": tokens,
+            "text": text,
+            "labels": labels,
+        }
+        result.append(datapoint)
+
+    train, rest = train_test_split(result, train_size=train_size, test_size=1-train_size)
+    test, dev = train_test_split(rest, train_size=test_size/(test_size+dev_size), test_size=dev_size/(test_size+dev_size))
+
+    srsly.write_jsonl(output_name+"_train.jsonl", train)
+    srsly.write_jsonl(output_name+"_test.jsonl", test)
+    srsly.write_jsonl(output_name+"_dev.jsonl", dev)
+
+
+def save_to_jsonl(dataset, output_name):
+    nlp = spacy.blank("en")
+    result = []
+    for example in dataset:
+        def replace_tag(tag_number: int):
+            return dataset.features["ner_tags"].feature.names[tag_number]
+
+        datapoint = {
+            "tokens": example["tokens"],
+            "text": " ".join(example["tokens"]),
+            "tags": [replace_tag(tag) for tag in example["ner_tags"]],
+        }
+
+        words_with_empty = example["tokens"]
+        words = []
+        for word in words_with_empty:
+            if word != "":
+                words.append(word)
+
+        spaces = len(words) * [True]
+        doc = Doc(nlp.vocab, words=words, spaces=spaces)
+
+        tmp = biluo_tags_to_spans(doc, iob_to_biluo(datapoint["tags"]))
+
+        char_spans = [(span.start_char, span.end_char, span.label_) for span in tmp]
+
+        datapoint["labels"] = char_spans
+        result.append(datapoint)
+
+    srsly.write_jsonl(output_name, result)
+
+def read_files_and_split():
+    result = []
+    jsonObj = pd.read_json(path_or_buf='your_path', lines=True)
+    result.extend(jsonObj.to_dict('records'))
+    jsonObj = pd.read_json(path_or_buf='your_path', lines=True)
+    result.extend(jsonObj.to_dict('records'))
+    
+    train, rest = train_test_split(result, train_size=0.7, test_size=0.3)
+    test, dev = train_test_split(rest, train_size=0.5, test_size=0.5)
+
+    srsly.write_jsonl("split_train.jsonl", train)
+    srsly.write_jsonl("split_test.jsonl", test)
+    srsly.write_jsonl("split_dev.jsonl", dev)
