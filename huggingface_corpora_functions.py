2b24775500f67bd051e32c73d1d0154334138935
Yokiball <yoka.kraemer@gmx.de>
2024-06-26 12:45:34 +0200

    Add huggingface functions

---
+++ huggingface_corpora_functions.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from datasets import load_dataset
+
+import argparse
+from huggingface_corpora_functions import load_corpus_from_huggingface, save_to_jsonl
+
+
+def load_corpus_from_huggingface(name: str, config: Optional[str] = None):
+    """ Loads Corpus from huggingface datasets library. List of available corpora: list_datasets()
+    Args:
+        - name (str): Name of the dataset
+        - str (str|None): config, if needed (e.g. 'nl' for dutch version of conll2002)
+    
+    Returns:
+        - tuple: train, dev, test set
+    """
+
+    if config:
+        corpus = load_dataset(name, config)
+    else:
+        corpus = load_dataset(name)
+
+    train = corpus["train"]
+    dev = corpus["validation"]
+    test = corpus["test"]
+
+    return (train, dev, test)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', help='Name of the corpus on huggingface')
+    parser.add_argument('--config', help="Configuration if needed for loading of corpus, default: None", default=None)
+    args = parser.parse_args()
+    train, dev, test = load_corpus_from_huggingface(args.name, args.config)
+
+    save_to_jsonl(train, "processed_data/"+args.name+"/train.jsonl")
+    save_to_jsonl(dev, "dev.jsonl")
+    save_to_jsonl(test, "test.jsonl")
+
+if __name__=="__main__":
+    main()
