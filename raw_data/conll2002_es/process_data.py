3badeee563f7b14fe0ca26e22e5c0fecf8221bfd
Yokiball <yoka.kraemer@gmx.de>
2024-06-27 08:55:32 +0200

    Add CoNLL 2002 NL and ES

---
+++ raw_data/conll2002_es/process_data.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:da9cdffd33ccb3a1a1b7fd368ff7ca4844429e5f99cc637134f8285136dd381c
+size 1751
---
+++ raw_data/conll2002_nl/process_data.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:80dc8c06490bed56c93eb788142abd891ab50e5334c42bc6caf70555bec8281d
+size 1751
