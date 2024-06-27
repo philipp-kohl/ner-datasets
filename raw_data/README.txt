15218188b88591e75ca46b38fe24d153157c58b5
Yokiball <yoka.kraemer@gmx.de>
2024-06-27 08:35:59 +0200

    Add SigIE

---
+++ raw_data/README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9b768ad6aaaff67ba8f6e7cb13b2256cce3887911086bc5cc1f8978098924c99
+size 1578
---
+++ raw_data/sigIE/process_data.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3566a2b26469e6bb5e005494d80786ef970091ce374f2e69758d63f3334ec400
+size 1623
